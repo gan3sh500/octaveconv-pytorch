6bb7439f0f3051d7daf8b390040629592fe3b79c
Ganesh Anand <gan3sh500@gmail.com>
2019-04-16 19:16:25 +0530

    first release

---
+++ README.md
@@ -0,0 +1,3 @@
+## Pytorch implementation of Octave Convolution
+This is an implementation of the paper [Drop an Octave: Reducing Spatial Redundancy in Convolutional Neural Networks with Octave Convolution](https://arxiv.org/abs/1904.05049). Works with version 1.0. 
+
---
+++ octconv.py
@@ -0,0 +1,24 @@
+import torch
+from functools import partial
+
+
+class OctConv(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, alpha):
+        super(OctConv, self).__init__()
+        self.kernel_size = kernel_size
+        self.L2L = torch.nn.Conv2d(int(alpha * in_channels), int(alpha * out_channels),
+                                   kernel_size, stride, kernel_size//2)
+        self.L2H = torch.nn.Conv2d(int(alpha * in_channels), out_channels - int(alpha * out_channels),
+                                   kernel_size, stride, kernel_size//2)
+        self.H2L = torch.nn.Conv2d(in_channels - int(alpha * in_channels), int(alpha * out_channels),
+                                   kernel_size, stride, kernel_size//2)
+        self.H2H = torch.nn.Conv2d(in_channels - int(alpha * in_channels),
+                                   out_channels - int(alpha * out_channels),
+                                   kernel_size, stride, kernel_size//2)
+        self.upsample = torch.nn.Upsample(scale_factor=2, mode='nearest')
+        self.avg_pool = partial(torch.nn.functional.avg_pool2d, kernel_size=kernel_size, stride=kernel_size)
+
+    def forward(self, x):
+        hf, lf = x
+        return self.H2H(hf) + self.upsample(self.L2H(lf)), self.L2L(lf) + self.avg_pool(hf)
+
